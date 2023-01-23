de6b4e00bfa460ae026eb9ff0a04bf3b2fe8c414
Nightowl <79215414+nightowl286@users.noreply.github.com>
2023-01-23 00:00:03 +0000

    Added the beginning of the writing libraries.

---
+++ src/writing/Writing.Abstractions/Entries/Components/IMessageComponentSerialiser.cs
@@ -0,0 +1,9 @@
+﻿using TNO.Logging.Common.Abstractions.Entries.Components;
+using TNO.Logging.Writing.Abstractions.Serialisers;
+
+namespace TNO.Logging.Writing.Abstractions.Entries.Components;
+
+/// <inheritdoc/>
+public interface IMessageComponentSerialiser : ISerialiser<IMessageComponent>
+{
+}
---
+++ src/writing/Writing/Entries/Components/MessageComponentSerialiser.cs
@@ -0,0 +1,15 @@
+﻿using TNO.Logging.Common.Abstractions.Entries.Components;
+using TNO.Logging.Writing.Abstractions.Entries.Components;
+
+namespace TNO.Logging.Writing.Entries.Components;
+
+internal sealed class MessageComponentSerialiser : IMessageComponentSerialiser
+{
+   #region Methods
+   public void Serialise(BinaryWriter writer, IMessageComponent data)
+   {
+      string message = data.Message;
+      writer.Write(message);
+   }
+   #endregion
+}
