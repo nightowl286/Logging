b0fc9509c7d24fd5daab362978b016c829ce417e
Nightowl <79215414+nightowl286@users.noreply.github.com>
2023-01-25 00:00:00 +0000

    Removed basic entries for now.

--- src/writing/Writing.Abstractions/Entries/IBasicEntrySerialiser.cs
+++
@@ -1,9 +0,0 @@
-﻿using TNO.Logging.Common.Abstractions.Entries;
-using TNO.Logging.Writing.Abstractions.Serialisers;
-
-namespace TNO.Logging.Writing.Abstractions.Entries;
-
-/// <inheritdoc/>
-public interface IBasicEntrySerialiser : ISerialiser<IBasicEntry>
-{
-}
