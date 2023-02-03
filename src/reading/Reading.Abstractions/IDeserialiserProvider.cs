ce343472adf657dad6b09bf47145da5b5cc90c61
Nightowl <79215414+nightowl286@users.noreply.github.com>
2023-02-03 00:00:00 +0000

    Added deserialiser provider.

---
+++ src/reading/Reading.Abstractions/IDeserialiserProvider.cs
@@ -0,0 +1,16 @@
+﻿using TNO.Logging.Reading.Abstractions.Deserialisers;
+
+namespace TNO.Logging.Reading.Abstractions;
+
+/// <summary>
+/// Denotes a provider for instances of <see cref="IDeserialiser"/>.
+/// </summary>
+public interface IDeserialiserProvider
+{
+   #region Methods
+   /// <summary>Gets a deserialiser of the type <typeparamref name="T"/>.</summary>
+   /// <typeparam name="T">The type of the deserialiser.</typeparam>
+   /// <returns>A deserialiser of the type <typeparamref name="T"/>.</returns>
+   T GetDeserialiser<T>() where T : notnull, IDeserialiser;
+   #endregion
+}
