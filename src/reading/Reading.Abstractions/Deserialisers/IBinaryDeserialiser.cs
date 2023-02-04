e1fc851520af07b154e2f9d0068eced6814a594e
Nightowl <79215414+nightowl286@users.noreply.github.com>
2023-02-04 00:00:00 +0000

    Code cleanup.

--- src/reading/Reading.Abstractions/Deserialisers/IBinaryDeserialiser.cs
+++ src/reading/Reading.Abstractions/Deserialisers/IBinaryDeserialiser.cs
@@ -9,7 +9,6 @@ namespace TNO.Logging.Reading.Abstractions.Deserialisers;
 public interface IBinaryDeserialiser<out T> : IDeserialiser
 {
    #region Methods
-
    /// <summary>Deserialises an instance of the type <typeparamref name="T"/> using the given <paramref name="reader"/>.</summary>
    /// <param name="reader">The reader to use.</param>
    /// <returns>The deserialised data of the type <typeparamref name="T"/>.</returns>
