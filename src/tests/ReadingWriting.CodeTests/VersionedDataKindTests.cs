09615ef856a316a3c410a5dd797157f427f90d7c
Nightowl <79215414+nightowl286@users.noreply.github.com>
2023-02-04 00:00:05 +0000

    Added code tests to ensure the data kind has been specified.

---
+++ src/tests/ReadingWriting.CodeTests/VersionedDataKindTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+using System.Text;
+using TNO.Common.Extensions;
+using TNO.Logging.Common.Abstractions;
+using TNO.Logging.Writing.Abstractions.Serialisers;
+
+namespace TNO.ReadingWriting.CodeTests;
+
+[TestClass]
+[TestCategory(Category.Versioning)]
+public class VersionedDataKindTests
+{
+   #region Methods
+   [TestMethod]
+   public void EnsureVersionedSerialisersHaveDataKindAttribute()
+   {
+      // Arrange
+      Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+      HashSet<Type> missing = new HashSet<Type>();
+
+      // Act
+      foreach (Type type in assemblies.SelectMany(a => a.DefinedTypes))
+      {
+         if (type.IsInterface == false)
+            continue;
+         bool isSerialiser = type.ImplementsOpenInterface(typeof(IBinarySerialiser<>));
+         bool isVersioned = type.GetInterfaces().Contains(typeof(IVersioned));
+
+         if (isSerialiser && isVersioned)
+         {
+            bool hasKinds =
+               type
+               .GetDataKinds()
+               .Any();
+
+            if (hasKinds == false)
+               missing.Add(type);
+         }
+      }
+
+      // Assert
+      if (missing.Count > 0)
+      {
+         StringBuilder messageBuilder = new StringBuilder();
+         messageBuilder
+            .AppendLine($"Some versioned serialisers were missing the {nameof(VersionedDataKindAttribute)}.")
+            .AppendLine()
+            .AppendLine($"Interfaces without an {nameof(VersionedDataKindAttribute)}:");
+
+         foreach (Type type in missing)
+            messageBuilder.AppendLine($"- {type.FullName}");
+
+         Assert.Fail(messageBuilder.ToString());
+      }
+   }
+   #endregion
+}
