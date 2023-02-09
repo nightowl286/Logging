52d9649bf92bff71834a14b9813a395e76179050
Nightowl <79215414+nightowl286@users.noreply.github.com>
2023-02-09 00:00:01 +0000

    Added the SeverityAndPurpose enum along with extension methods and 2 helper classes.

---
+++ src/common/Common.Abstractions/Entries/SeverityAndPurposeExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace TNO.Logging.Common.Abstractions.Entries;
+
+/// <summary>
+/// Contains useful extension methods related to the <see cref="SeverityAndPurpose"/>.
+/// </summary>
+public static class SeverityAndPurposeExtensions
+{
+   #region Methods
+   /// <summary>
+   /// Checks whether the given <paramref name="value"/> is logically
+   /// equivalent to <see cref="SeverityAndPurpose.None"/>.
+   /// </summary>
+   /// <param name="value">The value to check.</param>
+   /// <returns>
+   /// <see langword="true"/> if the given <paramref name="value"/> was logically
+   /// equivalent to <see cref="SeverityAndPurpose.None"/>, <see langword="false"/> otherwise.
+   /// </returns>
+   public static bool IsNone(this SeverityAndPurpose value)
+      => value == SeverityAndPurpose.None || value == SeverityAndPurpose.Empty;
+   #endregion
+}
