b368fab6388382f9dcd7b529591ed3eeef50669a
Nightowl <79215414+nightowl286@users.noreply.github.com>
2023-03-24 14:29:13 +0000

    Added serialisation unit tests.

---
+++ src/tests/ReadingWriting.Tests/Components/SimpleStackTraceComponentReadWriteTest.cs
@@ -0,0 +1,26 @@
+﻿using TNO.Logging.Common.Abstractions.Entries.Components;
+using TNO.Logging.Common.Entries.Components;
+using TNO.Logging.Writing.Entries.Components;
+
+namespace TNO.ReadingWriting.Tests.Components;
+
+[TestClass]
+[TestCategory(Category.Components)]
+public class SimpleStackTraceComponentReadWriteTest : ReadWriteTestBase<SimpleStackTraceComponentSerialiser, SimpleStackTraceComponentDeserialiserLatest, ISimpleStackTraceComponent>
+{
+   #region Methods
+   protected override ISimpleStackTraceComponent CreateData()
+   {
+      int threadId = 5;
+      string stackTrace = "some stack trace";
+      SimpleStackTraceComponent component = new SimpleStackTraceComponent(stackTrace, threadId);
+
+      return component;
+   }
+   protected override void Verify(ISimpleStackTraceComponent expected, ISimpleStackTraceComponent result)
+   {
+      Assert.That.AreEqual(expected.ThreadId, result.ThreadId);
+      Assert.That.AreEqual(expected.StackTrace, result.StackTrace);
+   }
+   #endregion
+}
---
+++ src/writing/Writing.Tests/BinarySerialiserCountTests/Components/SimpleStackTraceComponentSerialiserCountTests.cs
@@ -0,0 +1,22 @@
+﻿using TNO.Logging.Common.Abstractions.Entries.Components;
+using TNO.Logging.Common.Entries.Components;
+using TNO.Logging.Writing.Entries.Components;
+
+namespace TNO.Writing.Tests.BinarySerialiserCountTests.Components;
+
+[TestClass]
+[TestCategory(Category.Components)]
+public class SimpleStackTraceComponentSerialiserCountTests : BinarySerialiserCountTestBase<SimpleStackTraceComponentSerialiser, ISimpleStackTraceComponent>
+{
+   #region Tests
+   [TestMethod]
+   public void Count()
+   {
+      // Arrange
+      SimpleStackTraceComponent component = new SimpleStackTraceComponent("some stack trace", 1);
+
+      // Act + Assert
+      CountTestBase(component);
+   }
+   #endregion
+}
